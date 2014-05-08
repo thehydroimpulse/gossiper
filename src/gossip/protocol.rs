9b5d6d3500c094f30ab7d8d47ce3579e8eaa8e55
Daniel Fagnan <dnfagnan@gmail.com>
2014-05-07 18:53:06 -0600

    Parser
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

--- src/gossip/protocol.rs
+++ src/gossip/protocol.rs
@@ -3,3 +3,26 @@ pub enum Protocol {
     Binary(Vec<u8>),
     Text(~str)
 }
+
+pub enum TextProtocol {
+    Key(~str),
+    Value(Vec<u8>)
+}
+
+// The TextProtocol's parser. All we need to spit out is a key->value combination.
+// Because we allow streaming within the protocol, we'll only return each
+// key->value pair once we have fully parsed both.
+//
+// There could be an option to receive the raw data as we get it for the value,
+// but not the key.
+//
+// ```rust
+// Parser::new()
+// ```
+pub struct Parser {
+    input: Vec<u8>,
+    // This is the current character's position
+    position: 0,
+    curr_ch: '',
+    prev_ch: ''
+}
