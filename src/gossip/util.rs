671f007d9db734545c9aae159899c1822d1ae354
Daniel Fagnan <dnfagnan@gmail.com>
2014-05-30 23:40:48 -0600

    Updated error handling

--- src/gossip/util.rs
+++ src/gossip/util.rs
@@ -1,5 +1,6 @@
 use std::fmt::{Show,Formatter};
 use std::fmt;
+use std::io;
 
 pub fn as_byte_slice<'a, T>(x: &'a T) -> &'a [u8] {
     unsafe {
@@ -13,36 +14,37 @@ pub fn as_byte_slice<'a, T>(x: &'a T) -> &'a [u8] {
 
 pub type GossipResult<T> = Result<T, GossipError>;
 
-pub enum Error {
-    NotImplemented
-}
-
-macro_rules! unimplemented(
-    () => (
-        return Err(GossipError::new("not implemented".to_owned(), None))
-    )
-)
-
-/// A gossip error represents an error that happens typically during any I/O.
-///
-/// FIXME: Instead of a useless `code` property, it might be better to have
-///        that be an enum instead. With things like `FailedToJoin`.
-pub struct GossipError {
-    message: String,
-    error: Option<Error>
-}
-
-impl GossipError {
-    pub fn new(message: String, error: Option<Error>) -> GossipError {
-        GossipError {
-            message: message,
-            error: error
-        }
+/// Convert an IoError to a GossipError
+pub fn to_gossip(io: io::IoError) -> GossipError {
+    GossipError {
+        kind: IoError(io.clone()),
+        desc: StaticDescription(io.desc)
     }
 }
 
-impl Show for GossipError {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "{}", self.message)
+/// A gossip error represents an error that happens typically during any I/O.
+#[deriving(Show)]
+pub struct GossipError {
+    kind: GossipErrorKind,
+    desc: GossipErrorDescription,
+}
+
+#[deriving(Show)]
+pub enum GossipErrorDescription {
+    StaticDescription(&'static str),
+    BoxedDescription(String)
+}
+
+#[deriving(Show)]
+pub enum GossipErrorKind {
+    IoError(io::IoError)
+}
+
+impl GossipError {
+    pub fn new<T: Show>(desc: T, kind: GossipErrorKind) -> GossipError {
+        GossipError {
+            desc: BoxedDescription(desc.to_str()),
+            kind: kind
+        }
     }
 }
