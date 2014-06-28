45f15eafb0bc16b7210f47c1401583c5cb730ff2
Daniel Fagnan <dnfagnan@gmail.com>
2014-06-28 12:33:02 -0600

    Added some comments

--- src/gossip/lib.rs
+++ src/gossip/lib.rs
@@ -3,11 +3,20 @@
 #![feature(globs, macro_rules)]
 #![allow(unused_variable,unused_imports,unused_must_use,dead_code)]
 
-/*!
-Gossip protocol engine written in Rust.
-
-WIP
-*/
+//! Gossip.rs is a gossip/epidemic protocol based on the
+//! paper "Epidemic Broadcast Trees" in which it introduced
+//! the novel idea of the Plumtree.
+//!
+//! Gossip provides a way for a group of nodes, otherwise
+//! known as a cluster, to communicate and achieve
+//! consensus. However, it's specifically meant to be a
+//! highly available system (AP); thus, it supports
+//! eventual consistency in face of partitions.
+//!
+//! This trade-off is fine for many distributed systems
+//! that need to be highly available and fault-tolerant. Things
+//! such as data processing, analytics, etc... are all great
+//! candidates.
 
 extern crate collections;
 extern crate uuid;
