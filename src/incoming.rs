4321f6378f457e9f97b3204e9f647418625aa64c
Daniel Fagnan <dnfagnan@gmail.com>
2014-10-01 16:32:52 -0600

    Refactoring again

--- src/incoming.rs
+++ src/incoming.rs
@@ -29,6 +29,6 @@ mod tests {
 
     #[test]
     fn incoming() {
-        
+
     }
 }
--- src/node.rs
+++ src/node.rs
@@ -4,6 +4,7 @@ use uuid::Uuid;
 use addr::Addr;
 use result::{GossipResult, GossipError, NotListening};
 use broadcast::Broadcast;
+use incoming::Incoming;
 
 /// A peer describes a member within the cluster/network that
 /// is not the current one.
@@ -13,107 +14,69 @@ pub struct Peer {
     addr: Addr
 }
 
-/// Messages from the task to the user.
-#[deriving(Show, PartialEq)]
-pub enum TaskMessage {
-    Shutdown
-}
-
-pub enum Message {
-    BroadcastMessage(Broadcast),
-    NodeMessage(TaskMessage)
-}
-
-pub struct NodeTask {
-    receiver: Receiver<TaskMessage>,
-    senders: Vec<Sender<Broadcast>>,
-    task_msg_tx: Vec<Sender<TaskMessage>>
-}
-
-impl NodeTask {
-    pub fn new(tx: Sender<Broadcast>, task_msg_tx: Sender<TaskMessage>, rx: Receiver<TaskMessage>)
-        -> NodeTask {
-        NodeTask {
-            receiver: rx,
-            senders: vec![tx],
-            task_msg_tx: vec![task_msg_tx]
-        }
-    }
-
-    pub fn run(&mut self) {
-        loop {
-            match self.receiver.recv() {
-                Shutdown => {
-                    for sender in self.task_msg_tx.iter() {
-                        sender.send(Shutdown);
-                    }
-
-                    break;
-                }
-            }
-        }
-    }
-}
-
+/// A `Node` is a single member within the gossip protocol. Nodes that
+/// join together is called a cluster. This forms the distributed system
+/// in which the gossip protocol runs within.
+///
+/// Each Node is an equal member in the cluster. That means there isn't a
+/// single leader. This has a significant trade-off and one must understand
+/// it before being able to use the system correctly.
 pub struct Node {
+    /// Each node generates their own unique Uuid (v4) to uniquely
+    /// identify each other within the cluster. Instead of saying
+    /// "I'm node A", you would say "I'm node 123e4567-e89b-12d3-a456-426655440000".
     id: Uuid,
-    addr: Addr,
-    members: Vec<Peer>,
-    sender: Sender<TaskMessage>,
-    broadcast_rx: Receiver<Broadcast>,
-    task_msg_rx: Receiver<TaskMessage>
+
+    /// A set of other members within the cluster. This forms the basic
+    /// information about each Node. This doesn't, however, contain connection
+    /// information and what not.
+    members: Vec<Peer>
 }
 
 impl Node {
-
     /// Usage:
     ///
     /// ```rust
     /// use gossip::Node;
-    ///
-    /// // Bind the node to the specified address:
-    /// let mut node = Node::new("127.0.0.1", 5899);
-    ///
-    /// // Kill the server:
-    /// node.shutdown()
+    /// let mut node = Node::new();
     /// ```
-    pub fn new(ip: &str, port: u16) -> Node {
-        let (tx, rx) = channel();
-        let (broadcast_tx, broadcast_rx) = channel();
-        let (task_msg_tx, task_msg_rx) = channel();
-
-        TaskBuilder::new().named("NodeTask").spawn(proc() {
-            let (sender, receiver) = channel::<TaskMessage>();
-            tx.send(sender);
-
-            let mut task = NodeTask::new(broadcast_tx, task_msg_tx, receiver);
-            task.run();
-        });
-
+    pub fn new() -> Node {
         Node {
             id: Uuid::new_v4(),
-            addr: Addr::new(ip, port),
-            members: Vec::new(),
-            sender: rx.recv(),
-            broadcast_rx: broadcast_rx,
-            task_msg_rx: task_msg_rx
+            members: Vec::new()
         }
     }
 
-    /// Shutdown the current node.
-    pub fn shutdown(&mut self) {
-        self.sender.send(Shutdown);
+    /// Initialize the Node to listen on the specified address/port
+    /// combination. This will bootup the appropriate tasks to allow
+    /// incoming connections and broadcasts.
+    #[unimplemented]
+    pub fn listen(&mut self, host: &str, port: u16) -> GossipResult<()> {
+        Ok(())
     }
 
-    pub fn get_message(&mut self) {
+    /// Given a peer node, join it's existing cluster. Each node technically
+    /// creates their own cluster automatically. Joining multiple nodes together
+    /// is an explicit process. The peer node doesn't need to be the same one,
+    /// but it's not a bad idea.
+    #[unimplemented]
+    pub fn join(&mut self, host: &str, port: u16) -> GossipResult<()> {
+        Ok(())
     }
 
-    pub fn get_broadcast(&mut self) -> Broadcast {
-        self.broadcast_rx.recv()
-    }
+    /// Shutdown all the running tasks that are listening to new broadcasts
+    /// and incoming connections. This will send one last broadcast
+    /// to the current cluster notifying all other nodes of the shutdown.
+    ///
+    /// Afterwhich tasks will shutdown and the node will be terminated.
+    #[unimplemented]
+    pub fn shutdown(&mut self) {}
 
-    pub fn join(&mut self, host: &str, port: u16) {
-
+    /// Create a new `Incoming` iterator that iterates over newly received
+    /// broadcasts that the user can handle.
+    #[unimplemented]
+    pub fn incoming_iter(&self) -> Incoming {
+        Incoming::new(Vec::new())
     }
 }
 
